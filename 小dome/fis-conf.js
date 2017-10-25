7c105d339b1dd8c57b22878d47fce98c879cb5a2
liuxiaoran <mk9540@163.com>
2017-10-25 11:33:54 +0800

    0

---
+++ 小dome/fis-conf.js
@@ -0,0 +1,43 @@
+// 启用插件 
+fis.hook('relative'); 
+// 让所有文件，都使用相对路径。 
+fis.match('**', { relative: true })
+fis.match('**/common-template/**', { relative: false })
+
+
+// 加 md5
+// fis.match('*.{js,css,png,jpg}', {
+//   useHash: true
+// });
+
+// 启用 fis-spriter-csssprites 插件
+fis.match('::package', {
+  spriter: fis.plugin('csssprites')
+});
+
+// 对 CSS 进行图片合并
+fis.match('*.css', {
+  // 给匹配到的文件分配属性 `useSprite`
+  useSprite: true
+});
+
+fis.match('*.js', {
+  // fis-optimizer-uglify-js 插件进行压缩，已内置
+  optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.css', {
+  // fis-optimizer-clean-css 插件进行压缩，已内置
+  optimizer: fis.plugin('clean-css')
+});
+
+fis.match('*.png', {
+  // fis-optimizer-png-compressor 插件进行压缩，已内置
+  optimizer: fis.plugin('png-compressor')
+});
+
+fis.media('debug').match('*.{js,css,png}', {
+  useHash: false,
+  useSprite: false,
+  optimizer: null
+})

2fe49b8dfc2de21a3cb3a45320cab275a386bc6b
liuxiaoran <mk9540@163.com>
2017-11-10 14:32:01 +0800

    0

--- es6/src/1.js
+++
@@ -1,7 +0,0 @@
-
-let withinErrorMargin = (left,right) => {
-   return Math.abs(left - right) < Number.EPSILON * Math.pow(2, 2)
-}
-
-console.log(0.1+0.2 === 0.3)
-console.log(withinErrorMargin(0.1+0.2,0.3))

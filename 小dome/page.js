a33705243f13022ae7b7cf8f11fdf44762c94549
liuxiaoran <mk9540@163.com>
2018-01-29 10:02:11 +0800

    日常推送

---
+++ 小dome/page.js
@@ -0,0 +1,26 @@
+var AccountMonthBill = function () {
+	return this;
+}
+
+AccountMonthBill.prototype = {
+	init: function () {
+		this.pageInit();
+		this.bindEvent();
+	},
+	pageInit: function () {
+		var _this = this
+		
+    },
+    getDate: function(){
+        
+    },
+	bindEvent: function () {
+		var _this = this;
+
+		
+	},
+}
+
+$(function () {
+	new AccountMonthBill().init();
+})

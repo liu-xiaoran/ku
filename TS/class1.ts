f6d235f17643a224fea58a32fc0e922a3179eaa8
liuxiaoran <mk9540@163.com>
2018-02-24 11:46:05 +0800

    0

---
+++ TS/class1.ts
@@ -0,0 +1,14 @@
+
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+let greeter1 = new Greeter("world");
+
+console.log(greeter1.greet())
---
+++ TS/class2.ts
@@ -0,0 +1,16 @@
+class Animal {
+    move(distanceInMeters: number = 0) {
+        console.log(`Animals moved ${distanceInMeters}m.`);
+    }
+}
+
+class Dog extends Animal {
+    bark() {
+        console.log('Woof! Woof!');
+    }
+}
+
+const dog = new Dog();
+dog.bark();
+dog.move(10);
+dog.bark();
